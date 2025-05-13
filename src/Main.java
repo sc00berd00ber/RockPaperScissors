9397327eb7a920fa2e0b1510071f17e35d2b8cee
Nat Tessema <natnaeltessema1@gmail.com>
2025-05-12 23:46:55 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            Random random = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            String[] rps = {"rock", "paper", "scissors"};
+            String cpu = rps[random.nextInt(3)];
+            System.out.println("\nPick rock, paper, or scissors: \n");
+            System.out.println("Press 'X' to exit program\n");
+            String player = in.nextLine().toLowerCase().trim();
+            if (player.equalsIgnoreCase("x")){
+                break;
+            }
+            System.out.println("CPU chooses: " + cpu);
+
+            System.out.println("ROCK...PAPER...SCISSORS\n");
+
+            switch (player) {
+                case "rock":
+                    if (cpu.equalsIgnoreCase("rock")) {
+                        System.out.println("It's a Tie!");
+                        break;
+                    } else if (cpu.equalsIgnoreCase("paper")) {
+                        System.out.println("You lose!!");
+                        break;
+                    } else {
+                        System.out.println("You Win!!");
+                        break;
+                    }
+                case "paper":
+                    if (cpu.equalsIgnoreCase("paper")) {
+                        System.out.println("It's a Tie!");
+                        break;
+                    } else if (cpu.equalsIgnoreCase("scissors")) {
+                        System.out.println("You lose!!");
+                        break;
+                    } else {
+                        System.out.println("You Win!!");
+                        break;
+                    }
+                case "scissors":
+                    if (cpu.equalsIgnoreCase("scissors")) {
+                        System.out.println("It's a Tie!");
+                        break;
+                    } else if (cpu.equalsIgnoreCase("rock")) {
+                        System.out.println("You lose!!");
+                        break;
+                    } else {
+                        System.out.println("You Win!!");
+                        break;
+                    }
+            }
         }
     }
 }
